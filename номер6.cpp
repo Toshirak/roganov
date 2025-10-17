c756bec73964920cf74f57683e03764263ea6967
Toshirak <gleb.roganovv@yandex.ru>
2025-10-17 09:23:01 +0300

    6(1)cpp

--- номер6.cpp
+++ номер6.cpp
@@ -1,266 +1,96 @@
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
-using namespace std;
+#include <memory>      // std::unique_ptr
+#include <cstdlib>     // std::exit
 
-/**
- * @brief оператор выбоа способа заполнения массива
- * @param RANDOM = 0 автоматическое заполнение
- * @param MANUALLY =1 ручное заполнение
- */
-enum SELECT
+using std::cin;
+using std::cout;
+using std::cerr;
+
+// ---------- тип "динамический массив" ----------
+struct DynArray
 {
-    RANDOM = 0,
-    MANUALLY = 1
+    std::unique_ptr<int[]> data;
+    std::size_t            size{0};
+
+    explicit DynArray(std::size_t n)
+        : data(new int[n]), size(n) {}
 };
 
-/**
- * @brief проверяет размер массива
- * @param n - размер массива
- */
-void checkN(const int n);
-
-/**
- * @brief считывает размер массива
- * @return размер массива в беззнаковом типе данных
- */
-size_t getSize();
-
-/**
- * @brief считывает значение типа int
- * @return считанное значение целочисленное
- */
-int getNumber();
-
-/**
- * @brief вывод массива на экран
- * @param arr - массив
- * @param n - размер массива
- */
-void printArray(const int* arr, const int n);
-
-/**
- * @brief заполнение массива автоматически случайнвми числами в заданном диапазоне
- * @param arr - массив
- * @param n - размер массива
- * @param min - минимальное значение диапазона значений элементов массива
- * @param max - максимальное значение диапазона значений элементов массива
- */
-void fillArrayRandom(int* arr, const int n, const int min, const int max);
-
-/**
- * @brief проверяет диапазон
- * @param min - минимальное значение диапазона значений элементов массива
- * @param max - максимальное значение диапазона значений элементов массива
- */
-void checkRange(const int min, const int max);
-
-/**
- * @brief Заменяет минимальный элемент массива на средний, если количество элементов в массиве нечетное
- * @param arr  Массив целых чисел, в котором будет произведена замена
- * @param n Размер массива
- */
-int replaceMinWithAverage(int arr[], const int n);
-
-/**
- * @brief проверяет, содержит ли массив две пары соседних элементов с одинаковыми знаками.
- * @param arr Указатель на массив целых чисел.
- * @param n Количество элементов в массиве.
- * @return true, если в массиве есть две пары соседних элементов с одинаковыми знаками, иначе false.
- */
-bool hasTwoPairsWithSameSign(const int arr[], const int n);
-
-/**
- * @brief выводит индексы элементов массива, значения которых больше предыдущих элементов.
- * @param arr Указатель на массив целых чисел.
- * @param n Количество элементов в массиве.
- */
-void printIndicesGreaterThanPrevious(const int arr[], const int n);
-
-/**
- * @brief заполнение массива вручную
- * @param arr - массив
- * @param n - размер массива
- * @param min - минимальное значение диапазона значений элементов массива
- * @param max - максимальное значение диапазона значений элементов массива
- */
-void fillArray(int* arr, const int n, const int min, const int max);
-
-/**
-* @brief точка входа в программу
-* @return 0 - если программма выполнена корректно, инече -1
-*/
-int main()
+// ---------- ввод ----------
+std::size_t readSize()
 {
-    setlocale(LC_ALL, "Russian");
-    int n = getSize();
-    int* arr = new int[n];
-
-    cout << "Введите минимальное и максимальное значение диапазона: ";
-    int minValue = getNumber();
-    int maxValue = getNumber();
-    checkRange(minValue, maxValue);
-
-    cout << "Введите выбор для заполнения массива: " << endl
-        << RANDOM << " Для случайного заполнения" << endl
-        << MANUALLY << " Для ручного заполнения" << endl;
-
-    int choice = getNumber();
-
-    switch ((SELECT)choice)
-    {
-    case RANDOM:
-        fillArrayRandom(arr, n, minValue, maxValue);
-        break;
-    case MANUALLY:
-        fillArray(arr, n, minValue, maxValue);
-        break;
-    default:
-        cout << "Ваш выбор неверен" << endl;
-        return -1;
+    std::size_t n;
+    cout << "Введите кол-во элементов: ";
+    if (!(cin >> n) || n == 0) {
+        cerr << "Ошибка ввода: нужно натуральное число.\n";
+        std::exit(EXIT_FAILURE);
     }
-
-    cout << "Элементы массива:" << endl;
-    printArray(arr, n);
-
-    replaceMinWithAverage(arr, n);
-    printArray(arr, n);
-
-    printIndicesGreaterThanPrevious(arr, n);
-
-    if (hasTwoPairsWithSameSign(arr, n))
-    {
-        cout << "Есть две пары соседних элементов с одинаковыми знаками.\\n";
-    }
-    else
-    {
-        cout << "Две пары соседних элементов с одинаковыми знаками отсутствуют.\\n";
-    }
-
-    delete[] arr; // Освобождаем память массива
-    return 0;
+    return n;
 }
 
-void checkN(const int n)
+void readArray(DynArray& a)
 {
-    if (n <= 0)
-    {
-        cout << "Неправильный размер массива" << endl;
-        abort();
-    }
-}
-
-size_t getSize()
-{
-    cout << "Введите размер массива: ";
-    int n;
-    cin >> n;
-    checkN(n);
-    return (size_t)n;
-}
-
-int getNumber()
-{
-    int number;
-    cin >> number;
-    if (cin.fail())
-    {
-        cout << "Неправильный ввод данных";
-        abort();
-    }
-    return number;
-}
-
-void printArray(const int* arr, const int n)
-{
-    for (size_t i = 0; i < n; i++)
-    {
-        cout << "arr[" << i << "] = " << arr[i] << endl;
-    }
-}
-
-void fillArrayRandom(int* arr, const int n, const int min, const int max)
-{
-    srand(time(0));
-    for (size_t i = 0; i < n; i++)
-    {
-        arr[i] = rand() % (max - min + 1) + min;
-    }
-}
-
-void checkRange(const int min, const int max)
-{
-    if (min > max)
-    {
-        cout << "Введен неправильный диапазон" << endl;
-        abort();
-    }
-}
-
-int replaceMinWithAverage(int arr[], const int n)
-{
-    int minIndex = 0;
-
-    for (size_t i = 1; i < n; ++i)
-    {
-        if (arr[i] < arr[minIndex])
-        {
-            minIndex = i;
-        }
-    }
-
-    int sum = 0;
-
-    for (size_t i = 0; i < n; ++i)
-    {
-        sum += arr[i];
-    }
-
-    arr[minIndex] = sum / n;
-
-    cout << "Массив после замены минимального значения на среднее: ";
-    for (size_t i = 0; i < n; ++i)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
-void printIndicesGreaterThanPrevious(const int arr[], const int n)
-{
-    for (size_t i = 1; i < n; ++i)
-    {
-        if (arr[i] > arr[i - 1])
-        {
-            cout << "Индекс " << i << ": " << arr[i] << endl;
+    for (std::size_t i = 0; i < a.size; ++i) {
+        if (!(cin >> a.data[i])) {
+            cerr << "Ошибка ввода: ожидалось целое число.\n";
+            std::exit(EXIT_FAILURE);
         }
     }
 }
 
-bool hasTwoPairsWithSameSign(const int arr[], const int n)
+// ---------- вывод ----------
+void printArray(const DynArray& a)
 {
-    for (size_t i = 0; i < n - 3; ++i)
-    {
-        if ((arr[i] * arr[i + 1] > 0 && arr[i + 2] * arr[i + 3] > 0))
-        {
-            return true;
-        }
-    }
+    for (std::size_t i = 0; i < a.size; ++i) cout << a.data[i] << ' ';
+    cout << '\n';
+}
 
+// ---------- алгоритмы ----------
+std::size_t indexOfMin(const DynArray& a)
+{
+    if (a.size == 0) return 0;
+    std::size_t minIdx = 0;
+    for (std::size_t i = 1; i < a.size; ++i)
+        if (a.data[i] < a.data[minIdx]) minIdx = i;
+    return minIdx;
+}
+
+void replaceMinByMiddle(DynArray& a)
+{
+    a.data[indexOfMin(a)] = a.data[a.size / 2];
+}
+
+void printIncreasingIndexes(const DynArray& a)
+{
+    cout << "indexes: ";
+    for (std::size_t i = 1; i < a.size; ++i)
+        if (a.data[i] > a.data[i - 1]) cout << i << ' ';
+    cout << '\n';
+}
+
+bool hasTwoEqualSignPairs(const DynArray& a)
+{
+    std::size_t pairs = 0;
+    for (std::size_t i = 1; i < a.size; ++i)
+        if (a.data[i] * a.data[i - 1] >= 0 && ++pairs == 2) return true;
     return false;
 }
 
-void fillArray(int* arr, const int n, const int min, const int max)
+// ---------- main ----------
+int main()
 {
-    for (size_t i = 0; i < n; i++)
-    {
-        cout << "Enter value for arr[" << i << "]: ";
-        arr[i] = getNumber();
+    setlocale(LC_ALL, "rus");
 
-        if (arr[i] < min || arr[i] > max)
-        {
-            cout << "Значение вне диапазона" << endl;
-            i--;
-        }
-    }
-} 
+    DynArray arr(readSize());   // один объект — и всё
+    readArray(arr);
+    printArray(arr);
+
+    replaceMinByMiddle(arr);
+    printArray(arr);
+
+    printIncreasingIndexes(arr);
+
+    cout << "Две пары с одинаковыми знаками: "
+         << (hasTwoEqualSignPairs(arr) ? "есть\n" : "нет\n");
+    return 0;
+}
